47454d9cc7113b074b8e92caff5b456af1ff6192
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2025-07-09 20:17:32 +0300

    comment out unused harfbuzz dependency. if needed, comment it back on

--- src/shaper.rs
+++ src/shaper.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code)]
+// #![allow(dead_code)]
 #![allow(unused)]
 
 use crate::{logic::VariationKind, Variation};
@@ -16,52 +16,52 @@ pub trait Shaper<'f> {
     fn shape_text(&mut self, input: &str, variations: &[Variation]) -> Vec<GlyphData>;
 }
 
-pub(crate) struct HarfBuzz<'f>(harfbuzz_rs::Owned<harfbuzz_rs::Font<'f>>);
-impl<'f> HarfBuzz<'f> {
-    pub fn new(font_data: &'f [u8]) -> Self {
-        Self(harfbuzz_rs::Font::new(harfbuzz_rs::Face::from_bytes(font_data, 0)))
-    }
-}
-impl<'f> Shaper<'f> for HarfBuzz<'f> {
-    fn shape_text(&mut self, input: &str, variations: &[Variation]) -> Vec<GlyphData> {
-        let buffer = harfbuzz_rs::UnicodeBuffer::new().add_str(input);
-        self.0.set_variations(
-            &variations
-                .iter()
-                .filter_map(|v| match v.kind {
-                    VariationKind::Axis(tag) => {
-                        Some(harfbuzz_rs::Variation::new(&tag, v.current_value))
-                    }
-                    VariationKind::Spacing => None,
-                })
-                .collect::<Vec<_>>(),
-        );
+// pub(crate) struct HarfBuzz<'f>(harfbuzz_rs::Owned<harfbuzz_rs::Font<'f>>);
+// impl<'f> HarfBuzz<'f> {
+//     pub fn new(font_data: &'f [u8]) -> Self {
+//         Self(harfbuzz_rs::Font::new(harfbuzz_rs::Face::from_bytes(font_data, 0)))
+//     }
+// }
+// impl<'f> Shaper<'f> for HarfBuzz<'f> {
+//     fn shape_text(&mut self, input: &str, variations: &[Variation]) -> Vec<GlyphData> {
+//         let buffer = harfbuzz_rs::UnicodeBuffer::new().add_str(input);
+//         self.0.set_variations(
+//             &variations
+//                 .iter()
+//                 .filter_map(|v| match v.kind {
+//                     VariationKind::Axis(tag) => {
+//                         Some(harfbuzz_rs::Variation::new(&tag, v.current_value))
+//                     }
+//                     VariationKind::Spacing => None,
+//                 })
+//                 .collect::<Vec<_>>(),
+//         );
 
-        let output = harfbuzz_rs::shape(&self.0, buffer, &[]);
+//         let output = harfbuzz_rs::shape(&self.0, buffer, &[]);
 
-        let space = self.0.get_nominal_glyph(' ').expect("Font does not hace a space character.");
-        let space_width = self.0.get_glyph_h_advance(space);
-        let space_width = match variations.iter().find(|v| matches!(v.kind, VariationKind::Spacing))
-        {
-            Some(v) => (space_width as f32 * v.current_value) as i32,
-            None => space_width,
-        };
+//         let space = self.0.get_nominal_glyph(' ').expect("Font does not hace a space character.");
+//         let space_width = self.0.get_glyph_h_advance(space);
+//         let space_width = match variations.iter().find(|v| matches!(v.kind, VariationKind::Spacing))
+//         {
+//             Some(v) => (space_width as f32 * v.current_value) as i32,
+//             None => space_width,
+//         };
 
-        output
-            .get_glyph_infos()
-            .iter()
-            .zip(output.get_glyph_positions())
-            .map(|(i, p)| GlyphData {
-                codepoint: i.codepoint,
-                cluster: i.cluster,
-                x_advance: if i.codepoint == space { space_width } else { p.x_advance },
-                y_advance: p.y_advance,
-                x_offset: p.x_offset,
-                y_offset: p.y_offset,
-            })
-            .collect()
-    }
-}
+//         output
+//             .get_glyph_infos()
+//             .iter()
+//             .zip(output.get_glyph_positions())
+//             .map(|(i, p)| GlyphData {
+//                 codepoint: i.codepoint,
+//                 cluster: i.cluster,
+//                 x_advance: if i.codepoint == space { space_width } else { p.x_advance },
+//                 y_advance: p.y_advance,
+//                 x_offset: p.x_offset,
+//                 y_offset: p.y_offset,
+//             })
+//             .collect()
+//     }
+// }
 
 pub(crate) struct RustBuzz<'f>(rustybuzz::Face<'f>);
 impl<'f> RustBuzz<'f> {
